95cfe7c00356948ce9aa911189c52a179d1a7473
ParkMarie <park_marie@mail.ru>
2024-04-16 00:24:20 +0300

    Добавьте файлы проекта.

---
+++ Praktika3/BookStoreDataSet1.cs
@@ -0,0 +1,14 @@
+﻿namespace Praktika3
+{
+
+
+    public partial class BookStoreDataSet1
+    {
+    }
+}
+namespace Praktika3 {
+    
+    
+    public partial class BookStoreDataSet1 {
+    }
+}
---
+++ Praktika3/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Praktika3.BookStoreDataSet1TableAdapters;
+
+namespace Praktika3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OrderInfoTableAdapter OrderInfo = new OrderInfoTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            OrdersFullDataGrid.ItemsSource = OrderInfo.GetDataFullInfo();
+
+        }
+
+        private void New_Loaded(object sender, RoutedEventArgs e) 
+        {
+            OrdersFullDataGrid.Columns[0].Visibility = Visibility.Collapsed;
+            OrdersFullDataGrid.Columns[1].Visibility = Visibility.Collapsed;
+            OrdersFullDataGrid.Columns[2].Visibility = Visibility.Collapsed;
+            OrdersFullDataGrid.Columns[3].Visibility = Visibility.Collapsed;
+            OrdersFullDataGrid.Columns[6].Visibility = Visibility.Collapsed;
+            OrdersFullDataGrid.Columns[7].Visibility = Visibility.Collapsed;
+            OrdersFullDataGrid.Columns[8].Visibility = Visibility.Collapsed;
+            OrdersFullDataGrid.Columns[11].Visibility = Visibility.Collapsed;
+            
+
+        }
+
+    }
+}
